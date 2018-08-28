080692a45f15bb67d228a93686e43a21cf6d0518
benediktkauf <benediktkauf1993@gmail.com>
2018-08-28 12:05:25 +0200

    edits

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Personal Hompepage documentation build configuration file, created by
+# Personal Homepage documentation build configuration file, created by
 # sphinx-quickstart on Thu Aug 18 08:34:16 2016.
 #
 # This file is execfile()d with the current directory set to its
@@ -133,7 +133,8 @@ html_theme = 'alabaster'
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = u'Personal Hompepage v0.0.1'
+# html_title = u'Personal Hom
+epage v0.0.1'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
